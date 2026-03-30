50d8ad7fca5cc7efbdc347e754f5d4e90cdac0f6
Alexander Popov <alexander.popov.dev@gmail.com>
2026-03-30 20:38:13 +0300

    Add logs

--- comments/services.py
+++ comments/services.py
@@ -1,8 +1,11 @@
 import io
+import logging
 from pathlib import Path
 
 from django.core.files.base import ContentFile
 from PIL import Image
+
+logger = logging.getLogger(__name__)
 
 
 class CommentService:
@@ -12,9 +15,11 @@ class CommentService:
         img = Image.open(image)
         if img.width > max_width or img.height > max_height:
             img.thumbnail((max_width, max_height))
+            logger.info("Image %s resized to %sx%s", image.name, img.width, img.height)
             output = io.BytesIO()
             fmt = img.format or "JPEG"
             img.save(output, format=fmt)
             output.seek(0)
             return ContentFile(output.read(), name=Path(image.name).name)
+        logger.debug("Image %s is within bounds (%sx%s), skipping resize", image.name, img.width, img.height)
         return image
--- comments/signals.py
+++ comments/signals.py
@@ -1,3 +1,5 @@
+import logging
+
 from asgiref.sync import async_to_sync
 from channels.layers import get_channel_layer
 from django.core.cache import cache
@@ -9,6 +11,7 @@ from comments.serializers import CommentSerializer
 from .models import Comment
 from .tasks import comment_image_resize_task
 
+logger = logging.getLogger(__name__)
 channel_layer = get_channel_layer()
 
 
@@ -16,9 +19,11 @@ channel_layer = get_channel_layer()
 def on_comment_created(sender, instance, created, **kwargs):
     """On new comment: invalidate the comments list cache and trigger async image processing if needed."""
     if created:
+        logger.info("New comment created: id=%s", instance.id)
         cache.delete("comments_list")
         if instance.image_file:
             comment_image_resize_task.apply_async(kwargs={"comment_id": instance.id})
+            logger.info("Image resize task queued for comment %s", instance.id)
         async_to_sync(channel_layer.group_send)(
             "comments", {"type": "new_comment", "data": CommentSerializer(instance).data}
         )
