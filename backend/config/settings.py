4e2809422371401cb70c9bcae03cb5f570df8d27
Alexander Popov <alexander.popov.dev@gmail.com>
2026-04-08 11:32:27 +0300

    Uncomment Celery debug parameters

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -38,8 +38,8 @@ SECRET_KEY = config("SECRET_KEY")
 DEBUG = config("DEBUG", default=False, cast=bool)
 
 if DEBUG:
-    # CELERY_TASK_ALWAYS_EAGER = True
-    # CELERY_TASK_EAGER_PROPAGATES = True
+    CELERY_TASK_ALWAYS_EAGER = True
+    CELERY_TASK_EAGER_PROPAGATES = True
     CORS_ALLOW_ALL_ORIGINS = True
 
 ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="127.0.0.1,localhost", cast=Csv())
